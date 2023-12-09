c8ce5abc1c0832eba2576dd1571532ca99fbbe48
Aditya raj <meadityaraj0001@gmail.com>
2023-12-09 19:27:00 +0530

    feat: signup signin complete

--- packages/web/app/auth/page.tsx
+++ packages/web/app/auth/page.tsx
@@ -15,55 +15,54 @@ import {sign} from 'crypto';
 const Signup = ({setsigninpage, signinpage}) => {
   return (
     <>
-          <Card className="w-[80%]">
-            <CardHeader>
-              <CardTitle>Create Account</CardTitle>
-              {/* <CardDescription></CardDescription> */}
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="name">Email</Label>
-                    <Input id="name" placeholder="your working email address" />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Password</Label>
-                  <Input id="name" placeholder="setup a password" />
-                  </div>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button variant="outline" onClick={()=>setsigninpage(!signinpage)}>
-                Already a User -{`>`} Signin.
-              </Button>
-              <Button>Proceed</Button>
-            </CardFooter>
-          </Card>
+      <Card className=" p-0  w-[95%] lg:w-[80%]">
+        <CardHeader>
+          <CardTitle>Create Account</CardTitle>
+          {/* <CardDescription></CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Email</Label>
+                <Input id="name" placeholder="your working email address" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Password</Label>
+                <Input id="name" placeholder="setup a password" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline" onClick={() => setsigninpage(!signinpage)}>
+            Already a User -{`>`} Signin.
+          </Button>
+          <Button>Proceed</Button>
+        </CardFooter>
+      </Card>
     </>
   );
 };
 const Signin = ({setsigninpage, signinpage}) => {
   return (
     <>
-      
-          <Card className="w-[80%] ">
-            <CardHeader>
-              <CardTitle>Welcome Back</CardTitle>
-              {/* <CardDescription></CardDescription> */}
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="name">Email</Label>
-                    <Input id="name" placeholder="your working email address" />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">Password</Label>
-                  <Input id="name" placeholder="same as before" />
-                    {/* <Label htmlFor="framework">Password</Label>
+      <Card className="w-[95%] lg:w-[80%] ">
+        <CardHeader>
+          <CardTitle>Welcome Back</CardTitle>
+          {/* <CardDescription></CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Email</Label>
+                <Input id="name" placeholder="your working email address" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Password</Label>
+                <Input id="name" placeholder="same as before" />
+                {/* <Label htmlFor="framework">Password</Label>
                     <Select>
                       <SelectTrigger id="framework">
                         <SelectValue placeholder="Select" />
@@ -75,18 +74,17 @@ const Signin = ({setsigninpage, signinpage}) => {
                         <SelectItem value="nuxt">Nuxt.js</SelectItem>
                       </SelectContent>
                     </Select> */}
-                  </div>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button variant="outline" onClick={()=>setsigninpage(!signinpage)}>
-                Not a Registered User? -{`>`} Signup.
-              </Button>
-              <Button>Login</Button>
-            </CardFooter>
-          </Card>
-       
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline" onClick={() => setsigninpage(!signinpage)}>
+            Not a Registered User? -{`>`} Signup.
+          </Button>
+          <Button>Login</Button>
+        </CardFooter>
+      </Card>
     </>
   );
 };
@@ -96,22 +94,15 @@ const auth = () => {
 
   return (
     <>
-
-<div id="main" className="w-100vw h-screen relative flex flex-col items-center justify-center md:flex-row ">
-        <div id="left" className="w-full  h-screen absolute -z-10   bg-slate-700 md:static md:w-1/2 "></div>
+      <div id="main" className="w-100vw h-screen relative flex flex-col items-center justify-center md:flex-row ">
+        <div id="left" className="w-full bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2015&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] h-screen absolute -z-10   bg-slate-700 md:static md:w-1/2 "></div>
         <div id="right" className="w-full h-auto flex items-center justify-center md:w-1/2 ">
-        {!signinpage && <Signup setsigninpage={setsigninpage} signinpage={signinpage} />
-      }
-       {signinpage && <Signin setsigninpage={setsigninpage} signinpage={signinpage}></Signin>}
+          {!signinpage && <Signup setsigninpage={setsigninpage} signinpage={signinpage} />}
+          {signinpage && <Signin setsigninpage={setsigninpage} signinpage={signinpage}></Signin>}
         </div>
       </div>
-
-     
-     
-     
-     </>
- 
-  )
+    </>
+  );
 };
 
 export default auth;
--- packages/web/app/page.tsx
+++ packages/web/app/page.tsx
@@ -1,5 +1,3 @@
-import { link } from 'fs'
-import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Home() {
