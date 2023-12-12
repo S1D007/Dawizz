6e556a7ed4e3cdc02ffa6e00120b03f893ad4a3f
tarun-strrd <tarunsayyapureddi123@gmail.com>
2023-12-12 13:15:20 +0530

    feat(user): removed signout

--- packages/app/src/routers/auth/authRouter.ts
+++ packages/app/src/routers/auth/authRouter.ts
@@ -3,12 +3,8 @@ const router = express.Router();
 
 import signUpNewUser from '../../controller/auth/signUp';
 import signInUser from '../../controller/auth/signIn';
-import signOutUser from '../../controller/auth/signOut';
-
-console.log(typeof signUpNewUser, typeof signInUser, typeof signOutUser);
 
 router.post('/signup', signUpNewUser);
 router.post('/signin', signInUser);
-router.get('/signout', signOutUser);
 
 module.exports = router;
